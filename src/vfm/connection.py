f705907fa93bc411035793e5f35618e4a04f00c2
Priyanka Perera <priyanka.perera@cognite.com>
2025-12-03 03:12:01 +0530

    Fix project setup on Windows

--- src/vfm/connection.py
+++ src/vfm/connection.py
@@ -1,9 +1,77 @@
+from cognite.client import CogniteClient, ClientConfig
+from cognite.client.credentials import Token
+from msal import PublicClientApplication
+import pandas as pd
+
+TENANT_ID = "d9f7afd7-be4d-4a0f-9d22-10ebc305b615"
+CLIENT_ID = "d0b33cfc-f288-466e-84d0-a9508387b304"
+CDF_CLUSTER = "api"
+COGNITE_PROJECT = "production-optimisation-rd"
+BASE_URL = f"https://{CDF_CLUSTER}.cognitedata.com"
+SCOPES = [f"https://{CDF_CLUSTER}.cognitedata.com/.default"]
+AUTHORITY_HOST_URI = "https://login.microsoftonline.com"
+AUTHORITY_URI = AUTHORITY_HOST_URI + "/" + TENANT_ID
+PORT = 53000
+
 class Connection:
 
-    def get_client(self):
-        # Confidential
-        return None
+    def __init__(self):
+        self.__public_client_app = PublicClientApplication(client_id=CLIENT_ID, authority=AUTHORITY_URI)
 
-    def get_data(self, client, well='W06', start='2022-07-01', end='2022-09-01'):
-        # Confidential
-        return None
+    def get_client(self):
+        creds = self.__public_client_app.acquire_token_interactive(scopes=SCOPES, port=PORT)
+        cnf = ClientConfig(client_name="my-special-client", project=COGNITE_PROJECT,
+                           credentials=Token(creds["access_token"]), base_url=BASE_URL)
+        client = CogniteClient(cnf)
+        return client
+
+    def get_data(self, client: CogniteClient, well: str ='W06', start:int = None, end: int = None):
+        # # MPFM
+        # mpfm_wc = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_WC_MPFM", limit=None, start=start, end=end)
+        # mpfm_oil_rate = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_OIL_RATE_MPFM", limit=None, start=start, end=end)
+        # mpfm_gas_rate = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_GAS_RATE_MPFM", limit=None, start=start, end=end)
+
+
+        # # Well test
+        # wt_whp = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_WELLHEAD_PRESSURE", limit=None, start=start, end=end)
+        # wt_wht = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_WELLHEAD_TEMPERATURE", limit=None, start=start, end=end)
+        # wt_dhp = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_DOWNHOLE_PRESSURE", limit=None, start=start, end=end)
+        # wt_dht = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_DOWNHOLE_TEMPERATURE", limit=None, start=start, end=end)
+        # wt_gas_rate_test_sperator = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_GAS_RATE_TEST_SEPARATOR", limit=None, start=start, end=end)
+        # wt_water_rate_test_seperator = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_QW_TEST_SEPARATOR", limit=None, start=start, end=end)
+        # wt_oil_rate_test_seperator = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_OIL_RATE_TEST_SEPARATOR", limit=None, start=start, end=end)
+        # wt_gor_test_seperator = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_GOR_TEST_SEPARATOR", limit=None, start=start, end=end)
+        # wt_wc_test_seperator = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_WC_TEST_SEPARATOR", limit=None, start=start, end=end)
+        # wt_choke_downstream_pressure = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_DOWNSTREAM_CHOKE_PRES", limit=None, start=start, end=end)
+        # wt_choke_position = client.time_series.data.retrieve_dataframe(external_id=f"{well}_WELL_TEST_CHOKE_POS", limit=None, start=start, end=end)
+
+        mapping = {}
+        mapping["WHP"] = f"{well}_WELL_TEST_WELLHEAD_PRESSURE"
+        mapping["WHT"] = f"{well}_WELL_TEST_WELLHEAD_TEMPERATURE"
+        mapping["DHP"] = f"{well}_WELL_TEST_DOWNHOLE_PRESSURE"
+        mapping["DHT"] = f"{well}_WELL_TEST_DOWNHOLE_TEMPERATURE"
+        mapping["CHOKE"] = f"{well}_WELL_TEST_CHOKE_POS"
+        mapping["DCP"] = f"{well}_WELL_TEST_DOWNSTREAM_CHOKE_PRES"
+
+        mapping["QO_WELL_TEST"] = f"LER_Q_OIL_WELL_TEST_WELL{well[1:]}"
+        mapping["QG_WELL_TEST"] = f"LER_Q_GAS_WELL_TEST_WELL{well[1:]}"
+        mapping["QW_WELL_TEST"] = f"LER_Q_WATER_WELL_TEST_WELL{well[1:]}"
+
+        mapping["QO_MPFM"] = f"{well}_WELL_TEST_OIL_RATE_MPFM"
+        mapping["QG_MPFM"] = f"{well}_WELL_TEST_GAS_RATE_MPFM"
+        mapping["WC_MPFM"] = f"{well}_WELL_TEST_WC_MPFM"
+
+        # xids = [mapping[item] for item in
+        #         ["WHP", "DHP", "CHOKE", "DCP", "QO_MPFM", "GOR_MPFM", "WC_MPFM", "WHT", "WELL_TEST_OIL",
+        #          "WELL_TEST_WATER", "WELL_TEST_GAS", "DHT"]]
+
+        external_ids = list(mapping.values())
+        data = client.time_series.data.retrieve_dataframe(external_id=external_ids, start=start, end=end)
+        data = data.resample("1T").mean()
+        data = data.rename(columns={mapping[item]: item.lower() for item in mapping})
+        
+        # data = data.rename(columns={"whp": Feature.WELL_HEAD_PRESSURE.value, "dhp": Feature.DOWN_HOLE_PRESSURE.value, "dcp": Feature.DOWNSTREAM_CHOKE_PRESSURE.value,
+        #                             "wht": Feature.WELL_HEAD_TEMPERATURE.value, "dht": Feature.DOWN_HOLE_TEMPERATURE.value,
+        #                             "qo_mpfm": Feature.QO_MPFM.value})
+
+        return data
--- src/vfm/preprocessor.py
+++ src/vfm/preprocessor.py
@@ -11,28 +11,29 @@ class Preprocessor:
         df = self.__df_raw
 
         # Convert pressures to Pa
-        for item in ['dhp', 'whp', 'dcp']:
+        for item in ["dhp", "whp", "dcp"]:
             df[item] *= 1E5
 
         # Convert choke to fraction
-        df['choke'] /= 100
+        df["choke"] /= 100
 
         # Calculate MPFM flow rates from GOR and WC
-        df['qg_mpfm'] = df['qo_mpfm'] * df['gor_mpfm']
-        df['qw_mpfm'] = (df['wc_mpfm'] * df['qo_mpfm']) / (1 - df['wc_mpfm'])
+        df["gor_mpfm"] = df["qg_mpfm"] / df["qo_mpfm"]
+        df["qw_mpfm"] = (df["wc_mpfm"] * df["qo_mpfm"]) / (1 - df["wc_mpfm"])
 
         # Training data (based on well test)
-        fields_to_drop_na = ['qo_mpfm', 'qg_mpfm', 'qw_mpfm']
-        df = df[['dhp', 'dht', 'whp', 'wht', 'choke', 'dcp', 'qo_mpfm', 'qg_mpfm', 'qw_mpfm']]
+        fields_to_drop_na = ["qo_mpfm", "qg_mpfm", "qw_mpfm"]
+        # fields_to_drop_na = ["qo_well_test", "qg_well_test", "qw_well_test"]
+        df = df[["dhp", "dht", "whp", "wht", "choke", "dcp", "qo_mpfm", "qg_mpfm", "qw_mpfm", "qo_well_test", "qg_well_test", "qw_well_test"]]
         df = df.dropna(subset=fields_to_drop_na)
-        df = df.fillna(method='ffill')
-        return df[(df['choke'] > 0) & (df['qo_mpfm'] > 0) & (df['qg_mpfm'] > 0) & (df['qw_mpfm'] > 0)]
+        df = df.fillna(method="ffill")
+        return df[(df["choke"] > 0) & (df["qo_mpfm"] > 0) & (df["qg_mpfm"] > 0) & (df["qw_mpfm"] > 0)]
 
     def preprocess_timeseries(self):
         df = self.preprocess()
 
         # Calculate the time steps
         reference_time = df.index.min()  # You can choose any reference time you want
-        df['time_step'] = (df.index - reference_time).total_seconds() / 60
-        df['time_step'] = df['time_step'].astype(int)
+        df["time_step"] = (df.index - reference_time).total_seconds() / 60
+        df["time_step"] = df["time_step"].astype(int)
         return df
